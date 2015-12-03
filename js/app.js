48c33beff6391db6a595789b16a4e2ef1744accc
Brenno Ferrari <brennoferrari@gmail.com>
2015-12-03 17:15:40 -0200

    Angular-filter, unique filter, dropdowns por ordem alfab

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("TpApp", ['ui.router']);
+var app = angular.module("TpApp", ['ui.router', 'angular.filter']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   //
